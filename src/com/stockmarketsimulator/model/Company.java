811a07ce8c43e97a4a25fafb360228a562f2491a
Eliezer <eliezer1789j@gmail.com>
2020-05-04 16:22:23 +0100

    first commit - creating models Company and Investor

---
+++ src/com/stockmarketsimulator/model/Company.java
@@ -0,0 +1,33 @@
+package com.stockmarketsimulator.model;
+
+public class Company {
+	
+	private int id;
+	private int shares;
+	private int sharePrice;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getShares() {
+		return shares;
+	}
+	
+	public void setShares(int shares) {
+		this.shares = shares;
+	}
+	
+	public int getSharePrice() {
+		return sharePrice;
+	}
+	
+	public void setSharePrice(int sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+
+}
---
+++ src/com/stockmarketsimulator/model/Investor.java
@@ -0,0 +1,24 @@
+package com.stockmarketsimulator.model;
+
+public class Investor {
+	
+	private int id;
+	private int budget;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getBudget() {
+		return budget;
+	}
+	
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+
+}
