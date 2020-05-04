924eaeaf2855f04b00ac92484fca859e7a21e34a
Eliezer <eliezer1789j@gmail.com>
2020-05-04 17:26:34 +0100

    unit tests for share and budget generator methods

---
+++ src/com/stockmarketsimulator/unitTest/JavaUnitTest.java
@@ -0,0 +1,29 @@
+package com.stockmarketsimulator.unitTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.stockmarketsimulator.utils.RandomGenerator;
+
+public class JavaUnitTest {
+	
+	private RandomGenerator generator = new RandomGenerator();
+	
+	@Test
+	public void testGenerateShare() {
+		int min = 500;
+		int max = 1000;
+		int share = generator.getShare();
+		assertEquals(share >= min || share <= max, true);
+	}
+	
+	@Test
+	public void testGenerateBudget() {
+		int min = 1000;
+		int max = 10000;
+		int budget = generator.getBudget();
+		assertEquals(budget >= min || budget <= max, true);
+	}
+
+}
